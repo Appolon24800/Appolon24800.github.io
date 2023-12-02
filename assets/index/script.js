458581631461e515f6e11d1882ad56270a4fc9a6
Zakary GUERRET <93398824+Appolon24800@users.noreply.github.com>
2023-12-02 01:06:20 +0100

    sdsd
    sdsdsd

--- assets/index/script.js
+++ assets/index/script.js
@@ -1,21 +1,21 @@
 function title() {
   const titles = [
-      "Appolon.dev", "Appolon.de", "Appolon.d", "Appolon.",
-      "Appolon", "Appolo", "Appol", "Appo", "App", "Ap",
-      "A", "Ap", "App", "Appo", "Appol", "Appolo",
-      "Appolon", "Appolon.", "Appolon.d", "Appolon.de",
-      "Appolon.dev", "ppolon.dev", "polon.dev", "olon.dev",
-      "lon.dev", "on.dev", "o.dev", ".dev", "dev", "ev",
-      "v", "ev", "dev", "o.dev", "lon.dev", "olon.dev",
-      "polon.dev", "ppolon.dev", "Appolon.dev"
+    "Appolon.dev", "Appolon.de", "Appolon.d", "Appolon.",
+    "Appolon", "Appolo", "Appol", "Appo", "App", "Ap",
+    "A", "Ap", "App", "Appo", "Appol", "Appolo",
+    "Appolon", "Appolon.", "Appolon.d", "Appolon.de",
+    "Appolon.dev", "ppolon.dev", "polon.dev", "olon.dev",
+    "lon.dev", "on.dev", "o.dev", ".dev", "dev", "ev",
+    "v", "ev", "dev", "o.dev", "lon.dev", "olon.dev",
+    "polon.dev", "ppolon.dev", "Appolon.dev"
   ];
 
   let i = 0;
   document.title = titles[i];
 
   function update() {
-      i = (i + 1) % titles.length;
-      document.title = titles[i];
+    i = (i + 1) % titles.length;
+    document.title = titles[i];
   }
 
   setInterval(update, 100);
@@ -47,29 +47,29 @@ function randomnote() {
 function perror() {
   const params = new URLSearchParams(window.location.search);
   if (params.has('error')) {
-      ntf(params.get('error'))
+    ntf(params.get('error'))
   }
 }
 
 
 function sendmessage(message) {
   const req = {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-          'message': message
-      },
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'message': message
+    },
   };
   fetch('https://api.appolon.dev/index/message', req)
-      .then(response => {
-          return response.json();
-      })
-      .then(data => {
-          ntf('Message sent!')
-      })
-      .catch(error => {
-          ntf('Error: ', error)
-      })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      ntf('Message sent!')
+    })
+    .catch(error => {
+      ntf('Error: ', error)
+    })
 
   const inputElement = document.querySelector('.message');
   inputElement.value = '';
@@ -81,18 +81,18 @@ function ntf(message) {
   document.body.appendChild(notification);
 
   notification.innerHTML = message;
-  setTimeout(function() {
-      notification.style.top = "10px";
-      notification.style.opacity = "1";
+  setTimeout(function () {
+    notification.style.top = "10px";
+    notification.style.opacity = "1";
   }, 10);
 
-  setTimeout(function() {
-      notification.style.top = "-100px";
-      notification.style.opacity = "1";
+  setTimeout(function () {
+    notification.style.top = "-100px";
+    notification.style.opacity = "1";
   }, 5000);
 
-  setTimeout(function() {
-      document.body.removeChild(notification)
+  setTimeout(function () {
+    document.body.removeChild(notification)
   }, 5600);
 }
 
@@ -105,9 +105,9 @@ links.forEach(link => {
   });
 });
 
-window.mobileCheck = function() {
+window.mobileCheck = function () {
   let check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
   return check;
 };
 
@@ -146,15 +146,15 @@ function autoplayfix() {
   document.body.appendChild(text);
 
   function autoplayfix2() {
-      text.style.opacity = "0";
-      bg.style.opacity = "0";
-      setTimeout(() => {
-          document.body.removeChild(text);
-          document.body.removeChild(bg);
-          body.forEach(element => document.body.appendChild(element));
-      }, 500);
+    text.style.opacity = "0";
+    bg.style.opacity = "0";
+    setTimeout(() => {
+      document.body.removeChild(text);
+      document.body.removeChild(bg);
+      body.forEach(element => document.body.appendChild(element));
+    }, 500);
 
-      document.removeEventListener('click', autoplayfix2);
+    document.removeEventListener('click', autoplayfix2);
   }
 
   document.addEventListener('click', autoplayfix2);
